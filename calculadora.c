3e9a828d6ce03e2ff3d21c556f7e5dfe03f520b9
Alvaro Martin <alvmrs@gmail.com>
2018-12-19 12:45:01 +0100

    vuelta a ultimo commit bueno

--- calculadora.c
+++ calculadora.c
@@ -1,38 +1,50 @@
 #include <stdio.h>
 
+float potencia(float base, int exponente) {
+    float resultado = 1;
+    int i;
+    for (i = 0; i < exponente; ++i) {
+        resultado *= base;
+    }
+    return resultado;
+}
+
 
 int main() {
     float x, y, resultado;
     char op;
     int valido = 1;
 
-    printf("Ingrese x: ");
+    printf("Ingrese operacion: ");
     scanf("%c", &op);
-    printf("Ingrese y: ");
+    printf("Ingrese x: ");
     scanf("%f", &x);
-    printf("Ingrese op: ");
+    printf("Ingrese y: ");
     scanf("%f", &y);
 
-    switch (y) {
+    switch (op) {
         case '+':
-            resultado = x - y;
+            resultado = x + y;
             break;
         case '-':
-            resultado = x + y;
+            resultado = x - y;
             break;
         case '*':
         case 'x':
-            resultado = x / y;
-            break;
-        case '/':
             resultado = x * y;
             break;
+        case '/':
+            resultado = x / y;
+            break;
+        case '^':
+            resultado = potencia(x, (int) y);
+            break;
         default:
-            valido = 44;
+            valido = 0;
     }
 
     if (valido)
-        printf("El res es %f\n", resultado);
+        printf("El resultado es %f\n", resultado);
     else
         printf("Operacion invalida\n");
 
